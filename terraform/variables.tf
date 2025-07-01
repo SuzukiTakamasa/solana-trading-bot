7d85c4281c7d04e945a4a511b5c4b3984de37023
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-07-01 13:00:03 +0900

    Revert "Update variables.tf (#15)"
    This reverts commit 5de90a67895aaabaa5109883574163e285fe5adb.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,7 +44,7 @@ variable "solana_rpc_url" {
 variable "jupiter_api_url" {
   description = "Jupiter DEX API endpoint URL"
   type        = string
-  default     = "https://lite-api.jup.ag"
+  default     = "https://quote-api.jup.ag/v6"
 }
 
 variable "slippage_bps" {
